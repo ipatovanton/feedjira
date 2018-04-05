1c3d8b3739585d01ce98d2f6814b636393d009c9
Anton <ipatovanton@gmail.com>
2018-04-05 19:07:48 +0300

    Update atom_youtube_entry.rb

--- lib/feedjira/parser/atom_youtube_entry.rb
+++ lib/feedjira/parser/atom_youtube_entry.rb
@@ -19,8 +19,9 @@ module Feedjira
       element :"media:content", as: :media_type, value: :type
       element :"media:content", as: :media_width, value: :width
       element :"media:content", as: :media_height, value: :height
-      element :"media:thumbnail", as: :image_url, value: :url, with: {
-    type: "image/.*"
+      element :"media:thumbnail", as: :media_thumbnail_url, value: :url
+      element :"media:thumbnail", as: :media_thumbnail_width, value: :width
+      element :"media:thumbnail", as: :media_thumbnail_height, value: :height
       element :"media:starRating", as: :media_star_count, value: :count
       element :"media:starRating", as: :media_star_average, value: :average
       element :"media:statistics", as: :media_views, value: :views
